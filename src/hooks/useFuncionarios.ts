a8f07aeff21a514ef596cd85b10a9f9af801e439
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-09-26 12:32:04 -0300

    Verificar se o estado de funcionarios esta vazio

--- src/hooks/useFuncionarios.ts
+++ src/hooks/useFuncionarios.ts
@@ -4,6 +4,10 @@ import { FuncionariosContext } from "../contexts/FuncionariosContext"
 export function useFuncionarios(){
   const {criarFuncionario, funcionarioState: {data, isLoading}, getFuncionarios, deleteFuncionario} = useContext(FuncionariosContext);
   
+  if(data.length <= 0){
+    getFuncionarios();
+  }
+
   return {
     funcionarios: data,
     isLoading,
